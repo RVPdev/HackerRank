a17d150d692ea0a7815e50fffe4f51f9fb574452
RVPdev <renatovilcahuaman@gmail.com>
2023-11-18 15:11:24 -0600

    added solution for lonely integer

---
+++ Lonely_Integer.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    # Write your code here
+    for idx, i in enumerate(a):
+        count = 0
+        for j in a:
+            if j == i:
+                count += 1
+        if count <= 1:
+            return a[idx]
+    
+    return a[0]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
