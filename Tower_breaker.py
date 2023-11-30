91dc9daa1d0f7dfa28ae176222ccb80011e05825
RVPdev <renatovilcahuaman@gmail.com>
2023-11-30 12:23:33 -0600

    added solution for tower breaker

---
+++ Tower_breaker.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'towerBreakers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#
+
+def towerBreakers(n, m):
+    # If the number of towers is even or the height of towers is 1 (meaning no moves can be made),
+    # player 2 wins by default.
+    if n % 2 == 0 or m == 1:
+        return 2
+    else:
+        # Otherwise, player 1 has a winning strategy.
+        return 1
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        m = int(first_multiple_input[1])
+
+        result = towerBreakers(n, m)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
