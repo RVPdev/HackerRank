76872475abbd3284771f96fb2a6d5cfe279b97f3
RVPdev <renatovilcahuaman@gmail.com>
2024-01-10 16:57:03 -0600

    added comments and explanation for your solution

--- new_year_chaos.py
+++ new_year_chaos.py
@@ -15,7 +15,7 @@ import sys
 def minimumBribes(q):
     total_bribes = 0
 
-    # Iterate from the end of the queue to the front
+    # Inner loop: Check each person who could have bribed the person at position i
     for i in range(len(q) - 1, -1, -1):
         # Check if the current person has bribed more than two times
         if q[i] - (i + 1) > 2:
@@ -24,6 +24,12 @@ def minimumBribes(q):
 
         # Count how many times the current person has been bribed
         for j in range(max(0, q[i] - 2), i):
+            # max(0, q[i] - 2): Calculate the original position of the person at i, then go two positions back.
+            # This is because a person can only move forward by bribing up to two people.
+            # The max ensures we don't go to a negative index.
+
+            # Check if the person at position j (who was originally behind i) has a larger number.
+            # A larger number indicates they were originally further back in the queue and have moved ahead of i by bribing.
             if q[j] > q[i]:
                 total_bribes += 1
 
